93d8098fed9d4105f83cf93c8fa7a0103eeebb4b
sasmob <sasmob@SASMOBF6W-T03.SASMOB.OM>
2019-04-23 11:55:08 +0400

    Initial commit

---
+++ app/src/main/java/myapp/om/phone/SplashScreen.java
@@ -0,0 +1,29 @@
+package myapp.om.phone;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+    public static final int SPLASH_DELAY_MILLIS=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,PhoneAuthActivity.class));
+                finish();
+
+            }
+
+        },SPLASH_DELAY_MILLIS);
+    }
+}
